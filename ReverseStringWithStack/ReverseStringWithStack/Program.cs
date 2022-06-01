0467f2879a32e7c16af16510cf34a692d60d7f3e
Evinkaa27 <e.angelova27@gmail.com>
2022-06-01 21:01:07 +0300

    Solve 1 and 2 exams.

---
+++ ReverseStringWithStack/ReverseStringWithStack/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReverseStringWithStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> input = new Stack<char>(Console.ReadLine());
+            foreach (char c in input)
+            {
+                Console.Write(c);
+            }
+        }
+    }
+}
---
+++ StackSum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StackSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> forSum = new Stack<int>(input);
+            string[] commands = Console.ReadLine().ToLower().Split(' ');
+            
+            while (commands[0] != "end")
+            {
+                if (commands[0] == "add")
+                {
+                    forSum.Push(int.Parse(commands[1]));
+                    forSum.Push(int.Parse(commands[2]));
+                }
+                else if (commands[0] == "remove")
+                {
+                    if (int.Parse(commands[1]) < forSum.Count)
+                    {
+                        for (int i = 0; i < int.Parse(commands[1]); i++)
+                        {
+                            forSum.Pop();
+                        }
+                    }
+                }
+                commands = Console.ReadLine().ToLower().Split(' ');
+            }
+            Console.WriteLine($"Sum: {forSum.Sum()}");
+        }
+    }
+}
